c03397b6b50abc240450d80c54a08f5c7e4ed3e3
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-12 19:16:12 -0500

    Adds code

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,5 @@
-
-
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
-
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
-
-
-})
-
-
 describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
+  it("adds event listener to document", () => {
+    expect(document.addEventListener.calledOnce).to.be.true
   })
 })
