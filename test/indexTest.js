66e1ab6bf7c9088e9425c1863a869e1b6e4c369a
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-16 17:51:33 -0500

    Updates tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,15 @@
+const sinon = require('sinon');
+
 describe("index.js", () => {
-  it("adds event listener to document", () => {
+  it('has an eventlistener', function() {
+    sinon.spy(document, 'addEventListener')
+
+    domContentLoaded()
     expect(document.addEventListener.calledOnce).to.be.true
   })
+  it('should change the text on the page', function(){
+    expect(document.querySelector('p').textContent).to.equal("JavaScript is so cool. It lets me add text to my page programmatically.");
+    updateDOM()
+    expect(document.querySelector('p').textContent).to.equal("This is really cool!");
+  });
 })
