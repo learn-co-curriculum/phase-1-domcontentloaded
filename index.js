90b8f83172cc2a3982e41dc7949fcd75ab17b120
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-19 17:11:10 -0500

    Add solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    updateDOM()
+  });
+}
+
+function updateDOM() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
