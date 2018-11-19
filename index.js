0bff78cd77937cad59b9600830f0fb4219b7bb3d
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-19 17:11:10 -0500

    Add solution

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-document.addEventListener( "DOMContentLoaded", function () {
-  updateDOM()
-} );
-
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    updateDOM()
+  });
+}
 
 function updateDOM() {
-  document.getElementById( "text" )
-    .innerHTML = "This is really cool!";
+  document.getElementById("text").innerHTML = "This is really cool!";
 }
