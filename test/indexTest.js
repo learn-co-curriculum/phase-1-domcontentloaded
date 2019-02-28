010aca86f80a218acded9dd4591523c287caed62
maxwellbenton <maxwellbenton@gmail.com>
2019-02-28 08:14:24 -0500

    updates test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,16 @@
-const sinon = require('sinon');
+describe( "index.js", () => {
+  it( 'should change the text on the page when DOM content has loaded', function () {
+    expect( document.querySelector( 'p' )
+        .textContent )
+      .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
 
-describe("index.js", () => {
-  it('has an eventlistener', function() {
-    sinon.spy(document, 'addEventListener')
+    var event = document.createEvent( 'Event' );
+    event.initEvent( 'DOMContentLoaded', true, true );
+    window.document.dispatchEvent( event );
 
-    domContentLoaded()
-    expect(document.addEventListener.calledOnce).to.be.true
-  })
-  it('should change the text on the page', function(){
-    expect(document.querySelector('p').textContent).to.equal("JavaScript is so cool. It lets me add text to my page programmatically.");
-    updateDOM()
-    expect(document.querySelector('p').textContent).to.equal("This is really cool!");
-  });
-})
+    expect( document.querySelector( 'p' )
+        .textContent )
+      .to.equal( "This is really cool!" );
+  } )
+
+} )
