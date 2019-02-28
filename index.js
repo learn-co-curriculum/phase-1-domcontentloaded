643ecaf12228344e643c0452f9e6aa62371561ba
maxwellbenton <maxwellbenton@gmail.com>
2019-02-28 10:28:17 -0500

    updates solution

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function domContentLoaded() {
-  document.addEventListener("DOMContentLoaded", function(event) {
-    updateDOM()
-  });
-}
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
 
 function updateDOM() {
-  document.getElementById("text").innerHTML = "This is really cool!";
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
 }
