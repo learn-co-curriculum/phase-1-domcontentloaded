c09d0f1293d9fbe555224edec7a0693232dbcd4b
Hpollard003 <hpollard003@gmail.com>
2021-04-04 02:26:06 -0400

    commit tree 9acb64a8e7ac5bd28dd33029b4d556f073811c42

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function(){
+    let value = document.getElementById('text').textContent = 'This is really cool!'
+    return value
+})
