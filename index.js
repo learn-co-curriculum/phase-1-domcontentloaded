ca60f33b6a5917e05b0bc0f62ace51b52a69e93e
Serje <Serje.havandjian@gmail.com>
2022-04-11 10:47:09 -0400

    commit tree 13d5f4f8b93a51b670d24a3db4d1ba0d3e2cb088

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded")
+    document.getElementById("text").innerHTML = "This is really cool!"
+});
+
+console.log("TEST")
+
